e47cd31090aeee2f826bf65fa0cb5b9658b37d85
raisking <raiprakas007@gmail.com>
2017-09-15 09:13:42 -0400

    updating JS file

---
+++ js/appOne.js
@@ -0,0 +1,62 @@
+
+
+let css = {
+    Q1: "Which of the following defines a relative measurement for the height of a font in em spaces? hint'em'",
+    A1: "em",  
+    Q2: "Which of the following defines 1% of viewport width? hint: 'vh'",
+    A2: "vh",
+    Q3: "Which of the following property is used to set the width of anmage border? hint'border'",
+    A3: "border",
+    Q4: "Which of the following value of cursor shows it as the 'I' bar? hint'move'",
+    A4: "move",
+    Q5: "Which of the following property is used to set the text direction? hint 'direction'",
+    A5: "direction"
+ }
+let js = { 
+    Q1: "Which built-in method combines the text of two strings and returns a new string? hint 'concat()'",
+    A1: "concat()",
+    Q2: "Which of the following function of String object causes a string to be italic, as if it were in an <i> tag? hint'italics()'",
+    A2: "italics()",
+    Q3: "Which of the following function of String object extracts a section of a string and returns a new string? hint'slice()'",
+    A3: "slice()",
+    Q4: "Which of the following function of String object creates a string to be displayed as bold as if it were in a <b> tag? hint'bold()'",
+    A4: "bold()",
+    Q5: "Which of the following function of Array object adds one or more elements to the end of an array and returns the new length of the array? hint'push()'",
+    A5: "push()"
+}
+let java ={
+    Q1: "What is the size of byte variable? hint'8 bit'",
+    A1: "8 bit",
+    Q2: "Method Overriding is an example of - hint'Dynamic Binding'",
+    A2: "Dynamic Binding",
+    Q3: "Which method must be implemented by all threads? hint 'run()'",
+    A3: "run()",
+    Q4: "What is the default value of Boolean variable? hint 'false'",
+    A4: "false",
+    Q5: "What is the default value of byte variable? hint '0'",
+    A5: "0"   
+}
+let php ={
+    Q1: "Which of the following type of variables are floating-point numbers, like 3.14159 or 49.1? hint 'doubles'",
+    A1: "doubles",
+    Q2: "Which of the following array represents an array containing one or more arrays? hint 'Multidimentional Array'",
+    A2: "Multidimentional Array",
+    Q3: "Which of the following variable is used to get user's browser and operating system details in PHP? hint 'HTTP_USER_AGENT'",
+    A3: "HTTP_USER_AGENT",
+    Q4: "Which of the following method of Exception class returns array of the backtrace? hint 'getTrace()'",
+    A4: "getTrace()",
+    Q5: "Which of the following method can be used to create a MySql database using PHP? hint 'mysql_query()'",
+    A5: "mysql_query()"
+}
+let c ={
+    Q1: "Which of the following keyword is used for including the namespaces in the program in C#? hint'using'",
+    A1: "using",
+    Q2: "Which of the following converts a floating point or integer type to a decimal type in C#? hint 'ToDecimal'",
+    A2: "ToDecimal",
+    Q3: "Which of the following operator returns the type of a class in C#? hint'typeof'",
+    A3: "typeof",
+    Q4: "Which of the following property of Array class in C# gets a 64-bit integer, the total number of elements in all the dimensions of the Array? hint'LongLength'",
+    A4: "LongLength",
+    Q5: "Which of the following preprocessor directive allows you to undefine a symbol in C#? hint'undef'",
+    A5: "undef"
+}
